24352719ff5b4da40f255bdb68203e4fef9107db
raniel-roque <raniel.roque0509@gmail.com>
2026-01-29 09:41:09 +0800

    Added user type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             'first_name' => 'John',
             'last_name' => 'Doe',
             'username' => 'JDoe',
+            'user_type' => 1,
             'password' => bcrypt('password'),
         ]);
     }
