19e1e2cf3a059e223d45fd8cf398debf056d75fb
raniel-roque <raniel.roque0509@gmail.com>
2026-02-02 08:59:10 +0800

    Database Setup for Forms

---
+++ database/migrations/2026_02_02_085241_create_forms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('form_type_id')->constrained('form_types')->onDelete('cascade');
+            $table->json('form_inputs');
+            $table->timestamp('date_submitted')->nullable();
+            $table->foreignId('uploaded_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forms');
+    }
+};
