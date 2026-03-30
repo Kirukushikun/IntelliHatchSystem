9155dd4628a9009b854426e9648e99aa4cb17e9b
Adam <madamt0001@gmail.com>
2026-03-30 09:19:48 +0000

    update

--- routes/console.php
+++ routes/console.php
@@ -14,7 +14,7 @@ Schedule::command('backup:run --only-db')->dailyAt('02:00');
 Schedule::command('backup:clean')->dailyAt('02:30');
 
 // Schedule cleanup of orphaned photos every hour
-Schedule::command('app:cleanup-orphaned-photos')
+Schedule::command('photos:clean')
     ->hourly()
     ->description('Clean up orphaned photos that are not associated with any submitted form')
     ->onSuccess(function () {
